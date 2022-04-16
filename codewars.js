a515e5d23cd93ede77acfb2bbc7496a895f99722
Jameson Wang <jamesonw10@gmail.com>
2022-04-16 18:42:42 -0400

    edabit questions answered

---
+++ codewars.js
@@ -0,0 +1,27 @@
+////////////
+// Codewars
+////////////
+// // 1. Given a number "n", return the number of positive odd numbers below "n", EASY!
+// // oddCount(7) //=> 3, i.e [1, 3, 5]
+// // oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+
+// const oddCount = (n) => {
+//   Math.floor(n / 2);
+// };
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// // 2. Make a function that will return a greeting statement that uses an input; your program should return,
+// // ["Hello, <name> how are you doing today?"].
+// //(MAKE SURE YOU TYPE THE EXACT THING I WROTE OR THE PROGRAM MAY NOT EXECUTE PROPERLY)
+
+// const greet = (name) => {
+//   return `Hello, ${name} how are you doing today?`;
+// };
+
+// console.log(greet("Jameson"));
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// 3. https://www.codewars.com/kata/5748838ce2fab90b86001b1a/train/javascript
+// (skip)
---
+++ edabit.js
@@ -0,0 +1,16 @@
+//////////////////
+// EDABIT
+/////////////////
+// 1. HOW EDABIT WORKS
+const hello = () => {
+  return "hello edabit.com";
+};
+
+console.log(hello());
+
+// 2. Two Makes Ten
+const makesTen = (a, b) => {
+  return a === b;
+};
+
+console.log(makesTen());
