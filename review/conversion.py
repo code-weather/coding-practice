a6777c11ae3edca81284bba4bf1b9d736022f500
unknown <jamesonw10@gmail.com>
2023-12-30 23:33:07 -0500

    complete upper case conversion

--- review/conversion.py
+++ review/conversion.py
@@ -1,7 +1,14 @@
 def convert_to_upper(char):
   """ Complete this function with your solution """
-  if char.lower():
-    return char.upper()
+
+  ascii_value = ord(char)
+  lower_case = chr(ascii_value - 32)
+
+  if 97 <= ascii_value <= 122:
+    return lower_case
+  elif 65 <= ascii_value <= 90:
+    return char
+  return "?"
 
 def main ():
   print(convert_to_upper(str(input("Enter a value: "))))
